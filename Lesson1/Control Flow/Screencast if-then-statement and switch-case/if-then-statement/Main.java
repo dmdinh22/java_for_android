1a3bbf71b076ae15bf8642593e0d30d87303c2ce
David Dinh <dmdinh22@gmail.com>
2018-04-02 22:36:06 -0700

    control flow examples

--- Lesson1/Control Flow/Screencast if-then-statement and switch-case/if-then-statement/Main.java
+++ Lesson1/Control Flow/Screencast if-then-statement and switch-case/if-then-statement/Main.java
@@ -5,16 +5,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        String weather = "rainy";
-        //String weather = "sunny";
+        //String weather = "rainy";
+        String weather = "sunny";
         //String weather = "windy";
         //String weather = "snowy";
-        if(weather.equals("rainy")){
-            System.out.println("Don't forget to bring your umbrella");
-        }else if(weather.equals("sunny")){
-            System.out.println("Don't forget your sunglasses");
-        }else if(weather.equals("windy")){
-            System.out.println("Don't forget to bring your coat");
+        if(weather == "rainy"){
+            System.out.println("Don't forget to bring your umbrella.");
+        }else if(weather == "sunny"){
+            System.out.println("Don't forget your sunglasses.");
+        }else if(weather == "windy"){
+            System.out.println("Don't forget to bring your coat.");
         }else{
             System.out.println("Enjoy the snow!");
         }
--- Lesson1/Control Flow/Screencast if-then-statement and switch-case/switch-case-statement/Main.java
+++ Lesson1/Control Flow/Screencast if-then-statement and switch-case/switch-case-statement/Main.java
@@ -5,47 +5,47 @@
 public class Main {
 
     public static void main(String[] args) {
-        int birthMonth = 4;
+        int birthMonth = 7
         
         switch(birthMonth){
             case 1:
-                System.out.println("You where born in January");
-                break;
+                System.out.println("You where born in January.");
+                break; // breaks continuous evaluation
             case 2:
-                System.out.println("You where born in February");
+                System.out.println("You where born in February.");
                 break;
             case 3:
-                System.out.println("You where born in March");
+                System.out.println("You where born in March.");
                 break;
             case 4:
-                System.out.println("You where born in April");
+                System.out.println("You where born in April.");
                 break;
             case 5:
-                System.out.println("You where born in May");
+                System.out.println("You where born in May.");
                 break;
             case 6:
-                System.out.println("You where born in June");
+                System.out.println("You where born in June.");
                 break;
             case 7:
-                System.out.println("You where born in July");
+                System.out.println("You where born in July.");
                 break;
             case 8:
-                System.out.println("You where born in Agust");
+                System.out.println("You where born in Agust.");
                 break;
             case 9:
-                System.out.println("You where born in September");
+                System.out.println("You where born in September.");
                 break;
             case 10:
-                System.out.println("You where born in October");
+                System.out.println("You where born in October.");
                 break;
             case 11:
-                System.out.println("You where born in November");
+                System.out.println("You where born in November.");
                 break;
             case 12:
-                System.out.println("You where born in December");
+                System.out.println("You where born in December.");
                 break;
             default:
-                System.out.println("Invalid Month");
+                System.out.println("Invalid Month.");
         }
     }
 }
