bc1536c074710fb740a2932df6754c60c2698fc9
David Dinh <dmdinh22@gmail.com>
2018-04-03 23:36:09 -0700

    screencast of classes

--- Lesson1/Classes-objects and methods/Screencast of class objects and methods/Human.java
+++ Lesson1/Classes-objects and methods/Screencast of class objects and methods/Human.java
@@ -1,6 +1,6 @@
 import java.util.Date;
 
-public class Human{
+public class Human {
 	char gender; // 'F' or 'M'
 	String firstNames, lastName;
 	Human partner;
@@ -9,7 +9,7 @@ public class Human{
 	int monthBirth;
 	int dayBirth;
 
-	public Human(String firstNames, String lastName, char gender){
+	public Human(String firstNames, String lastName, char gender) {
 		this.firstNames = firstNames;
 		this.lastName = lastName;
 		this.gender = gender;
@@ -19,55 +19,56 @@ public class Human{
 		System.out.println(words);
 	}
 
-	public void addPartner(Human humanCouple){
+	public void addPartner(Human humanCouple) {
 		this.partner = humanCouple;
+		// set couple's parter to this object
 		humanCouple.partner = this;
 	}
 
-	public Human getPartner(){
+	public Human getPartner() {
 		return this.partner;
 	}
 
-	public void setId(int id){
+	public void setId(int id) {
 		identificationNumber = id;
 	}
 
-	public int getId(){
+	public int getId() {
 		return identificationNumber;
 	}
 
-	public void setBirthDate(int yearBirth, int monthBirth, int dayBirth){
+	public void setBirthDate(int yearBirth, int monthBirth, int dayBirth) {
 		this.monthBirth = monthBirth;
 		this.dayBirth = dayBirth;
 		this.yearBirth = yearBirth;
 	}
 
-	public int getAge(){
+	public int getAge() {
 		Date today = new Date();
 		
 		//the current year represented by this date, minus 1900
 		int currentYear = today.getYear() + 1900;
 		int age = currentYear - yearBirth;
 		
-		//0 is January, so add 1
+		//0 is January, so add 1 (0 index)
 		int currentMonth = today.getMonth() + 1;
-		if(currentMonth < monthBirth){
+		if (currentMonth < monthBirth) {
 			age--;
-		}else if(currentMonth == monthBirth){
+		} else if (currentMonth == monthBirth) {
 			//day of the Month
 			int currentDay = today.getDate();
-			if(dayBirth > currentDay){
+			if (dayBirth > currentDay) {
 				age--;
 			}
 		}
 		return age;
 	}
 
-	public String getPartnerName(){
+	public String getPartnerName() {
 		return this.partner.getFullName();
 	}
 
-	public String getFullName(){
+	public String getFullName() {
 		return this.firstNames + " " + this.lastName;
 	}
 
--- Lesson1/Classes-objects and methods/Screencast of class objects and methods/Main.java
+++ Lesson1/Classes-objects and methods/Screencast of class objects and methods/Main.java
@@ -5,15 +5,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        Human connie = new Human("Connie", "Smith", 'F');
-		connie.setBirthDate(1987,1,22); //January 22th, 1987
-		System.out.println(connie.getFullName() + " is "+ connie.getAge() + " years old");	
+        Human joe = new Human("Joe", "Cool", 'M');
+		joe.setBirthDate(1987,1,22); //January 22nd, 1987
+		System.out.println(joe.getFullName() + " is "+ joe.getAge() + " years old");
 
-		Human john = new Human("John", "Brown", 'M');
-		john.setBirthDate(1986,12,8);//December 08th, 1986
-		System.out.println(john.getFullName() + " is "+ john.getAge() + " years old");	
-		connie.addPartner(john);
-		System.out.println(connie.getFullName() + "'s partner is " +  connie.getPartnerName());
-		System.out.println(john.getFullName() + "'s partner is " +  john.getPartnerName());
+		Human oprah = new Human("Oprah", "Winfrey", 'M');
+		oprah.setBirthDate(1986,12,8);//December 08th, 1986
+		System.out.println(oprah.getFullName() + " is "+ oprah.getAge() + " years old");
+		joe.addPartner(oprah);
+		System.out.println(joe.getFullName() + "'s partner is " +  joe.getPartnerName());
+		System.out.println(oprah.getFullName() + "'s partner is " +  oprah.getPartnerName());
     }
 }
